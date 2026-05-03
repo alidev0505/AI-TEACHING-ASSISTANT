961741136bb9f3e326e728f05436473933eeb652
Ali Zaib <alizaibcomp@gmail.com>
2026-05-03 19:12:38 +0500

    updated api

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://investors-radius-worldcat-entrance.trycloudflare.com/api';
+const API_BASE_URL = 'https://functionality-congress-turtle-idea.trycloudflare.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
